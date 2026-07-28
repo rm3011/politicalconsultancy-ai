f6695bf80229633371a37dce726c859335f2f858
Harishna K <157455744+Harishna-K@users.noreply.github.com>
2026-07-28 11:02:31 +0530

    Add Google Tag Manager script to layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -155,17 +155,26 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 </Script>
       </head>
 
-      <body
-        className={`${inter.className} bg-[#050505] text-white antialiased`}
-      >
-        <noscript>
-  <iframe
-    src="https://www.googletagmanager.com/ns.html?id=GTM-59XN2T84"
-    height="0"
-    width="0"
-    style={{ display: "none", visibility: "hidden" }}
-  />
-</noscript>
+      <body className={`${inter.className} bg-[#050505] text-white antialiased`}>
+  <Script id="gtm-script" strategy="afterInteractive">
+    {`
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+      f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-59XN2T84');
+    `}
+  </Script>
+
+  <noscript>
+    <iframe
+      src="https://www.googletagmanager.com/ns.html?id=GTM-59XN2T84"
+      height="0"
+      width="0"
+      style={{ display: "none", visibility: "hidden" }}
+    />
+  </noscript>
         <a
           href="#main-content"
           className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-50 focus:px-4 focus:py-2 focus:bg-red-600 focus:text-white focus:rounded-lg"
