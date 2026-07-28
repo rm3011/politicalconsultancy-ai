5bb176849b59b6c0a91787753b68bb37ee760b0e
Harishna K <157455744+Harishna-K@users.noreply.github.com>
2026-07-28 10:21:08 +0530

    Integrate Google Tag Manager into layout
    Added Google Tag Manager script and noscript tag for tracking.

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -142,11 +142,22 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             }),
           }}
         />
+        <!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-59XN2T84');</script>
+<!-- End Google Tag Manager -->
       </head>
 
       <body
         className={`${inter.className} bg-[#050505] text-white antialiased`}
       >
+        <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-59XN2T84"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
         <a
           href="#main-content"
           className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-50 focus:px-4 focus:py-2 focus:bg-red-600 focus:text-white focus:rounded-lg"
